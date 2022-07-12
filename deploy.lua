d9aab3e21db56e02db4ea531d59c57c6ee748306
DoctorSigma <93322905+DoctorSigma@users.noreply.github.com>
2022-07-12 19:59:45 +0300

    Update "deploy.lua"

--- deploy.lua
+++ deploy.lua
@@ -1,70 +1,64 @@
-local listname = "/g4l.txt"
+local listname = "/Instructions.txt"
 local prefix = "https://raw.githubusercontent.com/"
--- string -> string
-function _GET(path)
+
+
+function _GET(path) --> status(bool), errorMsg(string), content -- Читает данные с ГитХаба
     local handle = http.get(prefix .. path)
-    if handle ==nil or handle.getResponseCode() ~= 200 then
-        return "", false
+	
+    if (handle == nil) or (handle.getResponseCode() ~= 200) then
+        return '"' .. path .. '" not responding', ""
     end
+	
     local content = handle.readAll()
     handle.close()
-    return content, true
+    return nil, content
 end
 
-function _DUMP(t)
-    for k,v in pairs(t) do
-        print(k, ":\t", v)
-    end
-end
-
-function clone(repo, branch) --> bool
+function clone(repo, branch) --> status(bool), errorMsg(string) -- Клонирует данные с ГитХаба
     local curdir = shell.dir() .. "/"
-    if branch == nil then
+    if branch == nil then -- Если в параметрах не была указана ветка, то устанавлвается значение по умолчанию, "master"
         branch = "master"
     end
 
-    if repo == nil then
-        if fs.exists(curdir .. listname) then 
-            h = fs.open(curdir .. listname, "r")
-            local s = h.readLine()
-            h.close()
-            if s ~= nil then
-                _, _, r, b = string.find(s, "([^%s]+)%s+([^%s]+)")
-                return clone(r, b)
-            end
+    if repo == nil then -- Если в параметрах не был указан репозиторий
+        if (fin = fs.open(curdir .. listname, "r")) ~= nil then -- Если в файлах на ПК есть файл инструкций, тоесть данная програма уже успешно выполнялась            
+			_, _, r = string.find(fin.readLine(), '!Repository="(+)"') -- Читаем первую строку, в которой должно находится имя репозитория
+			_, _, b = string.find(fin.readLine(), '!Branch="(+)"') -- Читаем следующую строку, в которой должна находится ветка
+            fin.close()
+            return clone(r, b)
         else
-            print("Please specify repository")
-            return false 
+            print("Please specify repository in arguments")
+            return false, "No repository name"
         end
     end
 
     local repopath = repo .. "/" .. branch .. "/"
-    local filelist, ok = _GET(repopath .. listname)
+    local ok, errorMsg, errorMsg, fileNameList = _GET(repopath .. listname)
 
     if not ok then 
         return (print("repository have no "..listname.." in the repo ".. repo) and false)
     end
 
     local first = true
-    for fname in string.gmatch(filelist, "([^\n]+)") do
+    for fname in string.gmatch(fileNameList, "([^\n]+)") do
         if first then 
-            first = false 
+			first = false 
         else
             print("Retrieving: ", fname)
-            local content, ok = _GET(repopath .. fname)
+            local ok, errorMsg, content = _GET(repopath .. fname)
             if not ok then 
-                print("..unexisted")
+                print("  ..unexisted")
             else
-                local h = fs.open(curdir .. fname, "w")
-                h.write(content)
-                h.close()
+                local fout = fs.open(curdir .. fname, "w")
+                fout.write(content)
+                fout.close()
             end
         end
     end
 end
 
 if false then
-local s, ok = _GET("Vany/gh4lua/master/README.md")
+local ok, errorMsg, s = _GET("Vany/gh4lua/master/README.md")
 print(s, ok)
 end
 
