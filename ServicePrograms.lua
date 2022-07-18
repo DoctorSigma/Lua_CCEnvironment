f5385287972cc44ce9080dd4af87dd500f90faa9
DoctorSigma <93322905+DoctorSigma@users.noreply.github.com>
2022-07-18 20:10:38 +0300

    Update ServicePrograms.lua

--- ServicePrograms.lua
+++ ServicePrograms.lua
@@ -1,4 +1,52 @@
-function goToGps_Recurse()
+local tFunctionLists = {} -- Таблица в которую будут добавлены функции, чтобы добавить напише TATBLE_NAME.FUNC_NAME() возле имени функции.
+
+
+function tFunctionLists.getTurtleDirection() --> status(bool), errorMsg(string), direction(vector) --No change position
+	local i = 1 -- Счётчик цыкла
+	local h = 0 -- Счётчик относительной высоты
+	
+-- Определяем наши координаты
+	local xPos, _, zPos = gps.locate(1)
+	if xPos == nil then return false, "I can't find gps!!!(start)", nil end -- Если не смогли определить местоположение
+-- Пробуем двигатся вперёд
+	while not turtle.forward() do -- Если черепах не смогла двинуться вперёд, то ...
+		if math.fmod(i, 4) == 0 then -- Если мы пробовали пройти вперёд уже 4 раза, то ..
+			i = 1 -- "обнуляем" счётчик
+			if turtle.up() -- Если мы сможем поднятся вверх, то..
+				h = h + 1
+			elseif turtle.down() -- Если мы не смогли поднятся вверх, но можем вниз, то ..
+				h = h - 1
+			else -- Мы не смогли никуда повернутся, ошибка
+				return false, "I can't move anywhere!!", nil 
+			end
+		else -- Если ещё не повернулись 4 раза, то ..		
+			turtle.turnRight()
+			i = i + 1
+		end
+	end
+	
+-- Определям новое местоположение
+	local xRel, _, zRel = gps.locate(1)
+	if xRel == nil then return false, "I can't find gps!!!(final)", nil end -- Если не смогли определить местоположение
+	
+-- "Обнуляем" набраную позицию
+	if not turtle.back() then return false, "I can't move back!!", nil end -- Возвращаемся назад, так как двигались вперёд
+	while h ~= 0 do -- Если мы двигались по вертикале, то пробуем обнулить набраную высоту
+		if h < 0 then 
+			if not turtle.up() then return false, "I can't move up!!", nil 
+			else h = h + 1 end
+		elseif h > 0 then
+			if not turtle.down() then return false, "I can't move down!!", nil 
+			else h = h - 1 end
+		end
+	end
+	
+-- Возвращаем направление
+	vDir = vector.new(xRel, 0, zRel) - vector.new(xPos, 0, zPos)
+	return true, "", vDir:normalize()
+end
+
+function tFunctionLists.goToGps_Recurse()
     h=0 --набраная высота
     
     axisX,axisY=0,0                             --поворот по осям х,у
@@ -67,3 +115,6 @@ function goToGps_Recurse()
     end
     
 end
+
+
+return tFunctionLists -- Возвращает таблицу, в которой находятся функции.
