cac003e22e030438d34040064345f5c54bd36f3b
DoctorSigma <93322905+DoctorSigma@users.noreply.github.com>
2022-07-24 19:00:54 +0300

    + Turtle_GeoFinder.lua - v1.4

--- User/Turtle_GeoFinder.lua
+++ User/Turtle_GeoFinder.lua
@@ -2,7 +2,7 @@
 local geoPeriphals = peripheral.wrap("right")
 local tArgs = { ... }
 
-print("GeoSearch programm, version: 1.4 \n")
+print("GeoFinder programm, version: 1.4 \n")
 
 if (#tArgs >= 1) and (geoPeriphals ~= nil) then -- Если в правой руке есть гео-сканер и было введено хотя бы один аргумент
     local tGeoScanRes, errorMsg = geoPeriphals.scan(tonumber(tArgs[1]))
