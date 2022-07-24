dbf0c7a8a7280526300fcc3bbdcce212e66bf4e0
DoctorSigma <93322905+DoctorSigma@users.noreply.github.com>
2022-07-24 18:47:58 +0300

    Test programm: geo scanner Version: 1.3.1

--- User/TestTurtle.lua
+++ User/TestTurtle.lua
@@ -2,12 +2,27 @@
 local geoPeriphals = peripheral.wrap("right")
 local tArgs = { ... }
 
-print("GeoSearch programm, version: 1.3.1 \n")
+print("GeoSearch programm, version: 1.3.2 \n")
 
 if (#tArgs >= 1) and (geoPeriphals ~= nil) then -- Если в правой руке есть гео-сканер и было введено хотя бы один аргумент
     local tGeoScanRes, errorMsg = geoPeriphals.scan(tonumber(tArgs[1]))
     if tGeoScanRes ~= nil then
-        local vPos = vector.new(gps.locate(1)) --ищем координаты
+
+        local vPos
+        if true then -- Просто отделили блоком
+            local xPos, yPos, zPos = gps.locate(1) --ищем координаты
+            if(xPos == nil) then -- Если нету GPS
+                print("GPS not found! Enter position manually:")
+                write("x:")
+                xPos = tonumber(read())
+                write("y:")
+                yPos = tonumber(read())
+                write("z:")
+                zPos = tonumber(read())
+            end
+            vPos = vector.new(xPos, yPos, zPos)
+        end
+
         if tArgs[2] == nil then tArgs[2] = "minecraft:ancient_debris" end
         for _, v in pairs(tGeoScanRes) do
             if (v.name == tArgs[2]) then --если это второй аргумент, то
