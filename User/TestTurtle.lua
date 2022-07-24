20870dd067138f24f6ec6d9211318696a6cf5803
DoctorSigma <93322905+DoctorSigma@users.noreply.github.com>
2022-07-24 18:01:27 +0300

    Test programm: geo scanner

--- User/TestTurtle.lua
+++ User/TestTurtle.lua
@@ -2,7 +2,7 @@ local fService = require("ServicePrograms")
 local geoPeriphals = peripheral.wrap("right")
 local tArgs = { ... }
 if (#tArgs > 0) and (geoPeriphals ~= nil) then -- Если в правой руке есть гео-сканер и было введено хотя бы один аргумент
-    local tGeoScanRes = geoPeriphals.scan(tArgs[1])
+    local tGeoScanRes = geoPeriphals.scan(tonumber(tArgs[1]))
     for _, v in pairs(tGeoScanRes) do
         textutils.pagedPrint(" ["..k.."] ".."Name: "..textutils.serialize(v, {}))
     end
