f64644b4dbbe2904e462c5a40a29e7b36d4a0b57
DoctorSigma <93322905+DoctorSigma@users.noreply.github.com>
2022-07-24 18:29:52 +0300

    Test programm: geo scanner Version: 1.3.1

--- User/TestTurtle.lua
+++ User/TestTurtle.lua
@@ -2,17 +2,19 @@
 local geoPeriphals = peripheral.wrap("right")
 local tArgs = { ... }
 
-print("GeoSearch programm, version: 1.3 \n")
+print("GeoSearch programm, version: 1.3.1 \n")
 
 if (#tArgs >= 1) and (geoPeriphals ~= nil) then -- Если в правой руке есть гео-сканер и было введено хотя бы один аргумент
-    local vPos = vector.new(gps.locate(1)) --ищем координаты
-
     local tGeoScanRes, errorMsg = geoPeriphals.scan(tonumber(tArgs[1]))
-    print(tGeoScanRes, errorMsg)
-    if tArgs[2] == nil then tArgs[2] = "minecraft:ancient_debris" end
-    for _, v in pairs(tGeoScanRes) do
-        if (v.name == tArgs[2]) then --если это второй аргумент, то
-            textutils.pagedPrint("x: " .. (v.x + vPos.x) .. " y:" .. (v.y + vPos.y) .. " z:" .. (v.z + vPos.z))
+    if tGeoScanRes ~= nil then
+        local vPos = vector.new(gps.locate(1)) --ищем координаты
+        if tArgs[2] == nil then tArgs[2] = "minecraft:ancient_debris" end
+        for _, v in pairs(tGeoScanRes) do
+            if (v.name == tArgs[2]) then --если это второй аргумент, то
+                textutils.pagedPrint("x:" .. (v.x + vPos.x) .. " y:" .. (v.y + vPos.y) .. " z:" .. (v.z + vPos.z))
+            end
         end
+    else
+        print(errorMsg)
     end
 end
