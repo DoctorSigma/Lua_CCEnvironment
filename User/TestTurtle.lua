9a93df992ead2582cf43457e27ce28754aa05155
DoctorSigma <93322905+DoctorSigma@users.noreply.github.com>
2022-07-24 18:20:25 +0300

    Test programm: geo scanner

--- User/TestTurtle.lua
+++ User/TestTurtle.lua
@@ -1,11 +1,17 @@
-local fService = require("ServicePrograms")
+--local fService = require("ServicePrograms")
 local geoPeriphals = peripheral.wrap("right")
 local tArgs = { ... }
-if (#tArgs >= 2) and (geoPeriphals ~= nil) then -- Если в правой руке есть гео-сканер и было введено хотя бы один аргумент
+
+print("GeoSearch programm, version: 1.2\n")
+
+if (#tArgs >= 1) and (geoPeriphals ~= nil) then -- Если в правой руке есть гео-сканер и было введено хотя бы один аргумент
+    local vPos = vector.new(gps.locate(1)) --ищем координаты
+
     local tGeoScanRes = geoPeriphals.scan(tonumber(tArgs[1]))
+    if tArgs[2] == nil then tArgs[2] = "minecraft:ancient_debris" end
     for _, v in pairs(tGeoScanRes) do
         if (v.name == tArgs[2]) then --если это второй аргумент, то
-            textutils.pagedPrint("x: " .. v.x .. " y:" .. v.y .. " z:" .. v.z)
+            textutils.pagedPrint("x: " .. (v.x + vPos.x) .. " y:" .. (v.y + vPos.y) .. " z:" .. (v.z + vPos.z))
         end
     end
 end
