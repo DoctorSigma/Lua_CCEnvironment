ecc35cf560c82cdcdf5d2fe031c2ab1a982009c1
DoctorSigma <93322905+DoctorSigma@users.noreply.github.com>
2022-11-06 11:27:46 +0200

    Rewriting: PC_AstralInfuserCrafter_WithIntegrated.lua Version: 1.2.1

--- User/PC_AstralInfuserCrafter_WithIntegrated.lua
+++ User/PC_AstralInfuserCrafter_WithIntegrated.lua
@@ -1,30 +1,20 @@
 local nbtStorage = peripheral.find("nbtStorage")
 local variableStore = peripheral.find("variableStore")
 local tInternData = { altarCoord = {x = -95, y = 70, z = -457}, starlightCoor = {
-    {x = -95, y = 69, z = -459},
-    {x = -96, y = 69, z = -459},
-    {x = -97, y = 69, z = -458},
-    {x = -97, y = 69, z = -457},
-    {x = -97, y = 69, z = -456},
-    {x = -96, y = 69, z = -455},
-    {x = -95, y = 69, z = -455},
-    {x = -94, y = 69, z = -455},
-    {x = -93, y = 69, z = -456},
-    {x = -93, y = 69, z = -457},
-    {x = -93, y = 69, z = -458},
-    {x = -94, y = 69, z = -459}
-}}
-local tOutData = {targetCoord = {x = 0, y = 0, z = 0}, clickWithEmpty = true, clickOnAltar = true}
+    {M = {x = -95, y = 69, z = -459}, L = {x = -94, y = 69, z = -459}, R = {x = -96, y = 69, z = -459}},
+    {M = {x = -97, y = 69, z = -457}, L = {x = -97, y = 69, z = -458}, R = {x = -97, y = 69, z = -456}},
+    {M = {x = -95, y = 69, z = -455}, L = {x = -94, y = 69, z = -455}, R = {x = -96, y = 69, z = -455}},
+    {M = {x = -93, y = 69, z = -457}, L = {x = -93, y = 69, z = -458}, R = {x = -93, y = 69, z = -456}} }}
+local tOutData = {targetCoordM = {x = 0, y = 0, z = 0}, targetCoordL = {x = 0, y = 0, z = 0}, targetCoordR = {x = 0, y = 0, z = 0}, isSuckLiquid = true, clickWithItemToCraft = true}
 local idHasItem = 0
 local fHasItemFlag = false
 local tArgs = { ... }
 
-print("#Name: PC_AstralInfuserCrafter_WithIntegrated.lua# || #Version: 1.1.5#\n")
+print("#Name: PC_AstralInfuserCrafter_WithIntegrated.lua# || #Version: 1.2.1#\n")
 
 if nbtStorage ~= nil then
     if variableStore ~= nil then
-        --TODO: забрати костиль, який виник через стару систему аргументыв запуску
-        if tArgs[1] == nil then tArgs[1] = "hasItem" end
+        if tArgs[1] == nil then tArgs[1] = "hasItem" end --TODO: забрати костиль, який виник через стару систему аргументів запуску
         if #tArgs >= 1 then
             -- Знаходимо айді змінної "Has item"
             for k, v in pairs(variableStore.list()) do
@@ -32,9 +22,14 @@ if nbtStorage ~= nil then
             end
 
             --Початкові налаштування
-            tOutData.targetCoord = tInternData.altarCoord
-            tOutData.clickWithEmpty = true
-            tOutData.clickOnAltar = true
+            tOutData.targetCoordM = tInternData.altarCoord
+            tOutData.targetCoordL = tInternData.altarCoord
+            tOutData.targetCoordR = tInternData.altarCoord
+            tOutData.isSuckLiquid = true
+            tOutData.clickWithItemToCraft = true -- Клікаємо предметом для крафту
+            nbtStorage.writeTable(tOutData)
+            sleep(0.5)
+            tOutData.clickWithItemToCraft = false -- Вимикаємо клік
             nbtStorage.writeTable(tOutData)
 
             while(true) do
@@ -45,21 +40,27 @@ if nbtStorage ~= nil then
                     fHasItemFlag = false -- Позначаємо, що предмета нема
                     -- Робимо "обхід" точок з світлом
                     for _, v in pairs(tInternData.starlightCoor) do
-                        tOutData.targetCoord = v
-                        tOutData.clickOnAltar = false
-                        tOutData.clickWithEmpty = true  --Спочатку пробуємо забрати пустим відром звідти рідину
+                        tOutData.targetCoordM = v.M
+                        tOutData.targetCoordL = v.L
+                        tOutData.targetCoordR = v.R
+                        tOutData.clickWithItemToCraft = false
+                        tOutData.isSuckLiquid = true  --Спочатку пробуємо забрати звідти рідину
                         nbtStorage.writeTable(tOutData)
-                        sleep(1)
-                        tOutData.clickWithEmpty = false --Ставимо рідину на місце тепер точно пустого місця
+                        sleep(0.5)
+                        tOutData.isSuckLiquid = false --Ставимо рідину на місце тепер точно пустого місця
                         nbtStorage.writeTable(tOutData)
-                        sleep(1)
+                        sleep(0.5)
                     end
                     -- Робимо йдемо на точку алтаря
-                    tOutData.targetCoord = tInternData.altarCoord
-                    tOutData.clickWithEmpty = true
-                    tOutData.clickOnAltar = true
+                    tOutData.targetCoordM = tInternData.altarCoord
+                    tOutData.targetCoordL = tInternData.altarCoord
+                    tOutData.targetCoordR = tInternData.altarCoord
+                    tOutData.isSuckLiquid = true
+                    tOutData.clickWithItemToCraft = true -- Клікаємо предметом для крафту
                     nbtStorage.writeTable(tOutData)
-                    sleep(1)
+                    sleep(0.5)
+                    tOutData.clickWithItemToCraft = false -- Вимикаємо клік
+                    nbtStorage.writeTable(tOutData)
                 end
             end
         else print('Usage:\n - "Has item" variable name') end
